473540e06427b828b2b2042da14c1571c0cc573c
lexadepuy03 <lexadepuy03@gmx.com>
2025-03-11 04:05:05 +0000

    Add 8.c

---
+++ src/8.c
@@ -0,0 +1,12 @@
+// My school project
+#include <stdio.h>
+
+int main() {
+  int age = 25;
+  if (age >= 18) {
+    printf("You are an adult!");
+  } else {
+    printf("You are not yet an adult.");
+  }
+  return 0;
+}
