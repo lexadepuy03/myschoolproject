ca1f85f2d2d4911143bec32f4e301798958d96c3
lexadepuy03 <lexadepuy03@gmx.com>
2025-03-13 10:35:05 +0000

    Add 9.c

---
+++ src/9.c
@@ -0,0 +1,6 @@
+  #include <stdio.h>
+  int main(){
+    //My school project is a great opportunity to learn new things and work on something meaningful.
+    printf("Hello world\n");
+    return 0;
+  }
