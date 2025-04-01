dee39ba49f0b112bd82c908065b4ea48d18b1297
lexadepuy03 <lexadepuy03@gmx.com>
2025-04-01 14:23:45 +0000

    Add 24.c

---
+++ src/24.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num = 5;
+    if (num > 0) {
+        printf("The number is positive.\n");
+    } else {
+        printf("The number is not positive.");
+    }
+
+    return 0;
+}
